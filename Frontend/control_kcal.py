8e05596c5b358a8cc0983b013003bc42d6e994d5
jUlsssS20 <pableoj816@gmail.com>
2024-04-20 23:26:36 +0800

    fixed validate button state

--- Frontend/control_kcal.py
+++ Frontend/control_kcal.py
@@ -2,9 +2,30 @@ import flet as ft
 import view_kcal as v
 import default as defa
 
-class Navigate:
+class ValidateButtonState:
     
-    def __init__(self, page: ft.Page):
-        self.page = page
+    def __init__(self, page, button, *fields):
+        
+        self.page       =   page
+        self.button     =   button
+        self.fields     =   fields
+        
+        for field in self.fields:
+            field.on_change = self.validate
+    
+    def validate(self, *args):
+        
+        if all(field.value for field in self.fields):
+                self.button.disabled = False
+                
+        else:
+                self.button.disabled = True
+        
+        self.page.update()
+        
+        
+        
+         
+
     
     
